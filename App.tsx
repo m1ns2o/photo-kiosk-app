285bdc55873ff6b87bdd40b14dfae1428cd2510a
m1ns2o <srjms0128@gmail.com>
2023-12-25 16:21:51 +0900

    safe area ios&aos

--- App.tsx
+++ App.tsx
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StyleSheet } from "react-native";
+import {
+	SafeAreaProvider,
+	SafeAreaView,
+	useSafeAreaInsets,
+} from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+
+function HomeScreen() {
+	const insets = useSafeAreaInsets();
+	return (
+		// <View
+		// 	style={{
+		// 		...styles.container,
+		// 		paddingTop: insets.top,
+		// 		paddingBottom: insets.bottom,
+		// 	}}
+		// >
+		<SafeAreaView>
+			
+			<Text style={{ fontSize: 28 }}>Content is in safe area.</Text>
+		</SafeAreaView>
+		// </View>
+	);
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+	return (
+		<SafeAreaProvider>
       <StatusBar style="auto" />
-    </View>
-  );
+			<HomeScreen />
+		</SafeAreaProvider>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "flex-end",
+	},
 });
